d9894d4971a65f49b6e6c82cfcc8bf479aa35d22
BeatDiaz <beatdiazmusic@gmail.com>
2025-07-01 14:10:56 +0300

    read func and update func

--- userManager.js
+++ userManager.js
@@ -1,23 +1,89 @@
 import fs from "fs";
+import { json } from "stream/consumers";
 
-const read = function(path){
-    fs.readFile(path,"utf-8", (err, data) => {
+const create = function (path, str) {
+    fs.readFile(path, "utf-8", (err, data) => {
         if (err) {
             console.log(err);
             return;
         }
-        console.log(data);
-    });
-}
+        const arrData = JSON.parse(data);
+        arrData.push(str);
+        const newData = JSON.stringify(arrData);
 
-const create = function(path, data){
-    fs.appendFile(path, data, (err) => {
+        fs.writeFile(path, newData, (err) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log(`Data added successfully`);
+        });
+    });
+};
+
+
+const read = function (path) {
+    fs.readFile(path, "utf-8", (err, data) => {
         if (err) {
             console.log(err);
             return;
         }
-        console.log(`Data added successfully`);
+        console.log(JSON.parse(data));
     });
 }
 
-const 
+const update = function (path, exsitsStr, upStr) {
+    fs.readFile(path, "utf-8", (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        const arrData = JSON.parse(data);
+        if (!arrData.includes(exsitsStr)) {
+            console.log(`The <${exsitsStr}> not exsits in the file`);
+            return;
+        }
+
+        const newData = JSON.stringify(
+            arrData.map(str => {
+                if (str === exsitsStr) {
+                    console.log(str);
+                    return upStr;
+                } else return str;
+            })
+        );
+
+        fs.writeFile(path, newData, (err) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log(`Data update successfully: (${exsitsStr} with ${upStr})`);
+        })
+    });
+}
+
+const deleteStr = function (path, deStr) {
+    fs.readFile(path, "utf-8", (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        const arrData = JSON.parse(data);
+        if (!arrData.includes(deStr)) {
+            console.log(`The <${deStr}> not exsits in the file`);
+            return;
+        }
+
+    });
+}
+
+const path = "DB/db.txt"
+// create(path, "yona");
+// update(path, "tami", "noam");
+setTimeout(() => {
+    read("DB/db.txt")
+}, 2000)
+
